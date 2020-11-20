2b62c20e6abbe1490de894aef417519dcc65d68a
balaji <balaji17112000@gmail.com>
2020-11-20 17:04:51 +0530

    Updates

---
+++ precache-manifest.d5961f87575fed87ad44a6b93a2e5f1f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ef6fcc24c2f9b541f3b8105e415af1",
+    "url": "/Restaurant-System/index.html"
+  },
+  {
+    "revision": "1c4731db8fac5a171aab",
+    "url": "/Restaurant-System/static/css/2.748ab408.chunk.css"
+  },
+  {
+    "revision": "0b2eb06d2ba894ca2e1f",
+    "url": "/Restaurant-System/static/css/main.9d825108.chunk.css"
+  },
+  {
+    "revision": "1c4731db8fac5a171aab",
+    "url": "/Restaurant-System/static/js/2.d5245af9.chunk.js"
+  },
+  {
+    "revision": "054ca767c3d6c1d37d76bc942cceddd0",
+    "url": "/Restaurant-System/static/js/2.d5245af9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b2eb06d2ba894ca2e1f",
+    "url": "/Restaurant-System/static/js/main.e920451d.chunk.js"
+  },
+  {
+    "revision": "f74959126996b883db02",
+    "url": "/Restaurant-System/static/js/runtime-main.186d6631.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Restaurant-System/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Restaurant-System/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Restaurant-System/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Restaurant-System/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Restaurant-System/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
