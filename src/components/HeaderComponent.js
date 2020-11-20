9bab923b31bd8a0565f35c209643040a428de1c1
balaji <72458396+balaji17112000@users.noreply.github.com>
2020-11-20 17:41:31 +0530

    Update HeaderComponent.js

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -62,7 +62,7 @@ class Header extends Component {
                 <Navbar dark expand="md">
                     <div className="container">
                         <NavbarToggler onClick={this.toggleNav} />
-                        <NavbarBrand className="mr-auto" href="/"><img src='assets/images/logo.png' height="30" width="41" alt='Balaji Restaurent' /></NavbarBrand>
+                        <NavbarBrand className="mr-auto" href="/"><img src='Restaurant-System/public/assets/images/logo.png' height="30" width="41" alt='Balaji Restaurent' /></NavbarBrand>
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
                             <NavItem>
